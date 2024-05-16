044abdd22c681afbf1183bbfa9696e53ce79a8b4
evan <evanmhelle@gmail.com>
2024-05-16 10:51:27 +0200

    connexion : mise en place des forms

---
+++ connexion.php
@@ -0,0 +1,24 @@
+<?php 
+    include("./../Projet_Pixel_War_Helle_Evan/include/config.inc.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Veuillez vous identifier</h1>
+    <form method="POST">
+        <h3>entrez un pseudo</h3>
+        <input type="text" name="pseudo" value="" required>
+        <h3>entrez un mot de passe</h3>
+        <input type="password" name="password" value="" required>
+        <input type="submit" value="S'enregister">
+        </form>
+    
+</body>
+</html>
--- creation_compte.php
+++ creation_compte.php
@@ -21,7 +21,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Enregistrement</title>
 </head>
 <body>
     <h1>Création de compte</h1>
