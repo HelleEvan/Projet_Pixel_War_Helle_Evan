a3f6349f392304693b2e6ba0922cc2ae4a7c4867
evan <evanmhelle@gmail.com>
2024-05-21 19:47:53 +0200

    coloriage de la grille possible avec 3 couleurs pour l'instant

--- js/grille.js
+++ js/grille.js
@@ -8,17 +8,28 @@ function generateGrille(){
 
 }
 window.addEventListener("load",generateGrille());
-
+//changer la couleur d'un pixel
 function color(){
     const pixels = Array.from(document.querySelectorAll("pixel"));
+    const colors_display = Array.from(document.querySelectorAll("color"));
     pixels.forEach(pixel => {
-        pixel.addEventListener("click",()=>{
-            pixel.classList.add("red");
+        colors_display.forEach(color =>{
+            pixel.addEventListener("click",()=>{
+                if(color.classList.contains("red")&&color.classList.contains("active")){
+                    pixel.classList.add("red");
+                }
+                if(color.classList.contains("green")&&color.classList.contains("active")){
+                    pixel.classList.add("green");
+                }
+                if(color.classList.contains("blue")&&color.classList.contains("active")){
+                    pixel.classList.add("blue");
+                }
+            });
         });
     });
 }
 window.addEventListener("load",color());
-
+//selection de couleur
 function color_choice(){
     const colors_display = Array.from(document.querySelectorAll("color"));
     colors_display.forEach(color => {
